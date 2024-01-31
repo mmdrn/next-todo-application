888216b7912a8463b83e5731838464121fbc060a
Mohammad RN <mr.nezamdust@gmail.com>
2024-01-31 19:20:46 +0330

    deleteHandler has been added to the todo component

--- components/todo/index.tsx
+++ components/todo/index.tsx
@@ -1,6 +1,12 @@
 import { Props } from "./types";
 
-export default function Todo({ date, title, status, clickHandler }: Props) {
+export default function Todo({
+  date,
+  title,
+  status,
+  clickHandler,
+  deleteHandler,
+}: Props) {
   return (
     <article
       className="rounded-md px-2 py-2 bg-slate-50 border border-slate-300 cursor-pointer hover:border-blue-500 transition-colors grid grid-cols-4 gap-4 w-full"
@@ -10,12 +16,26 @@ export default function Todo({ date, title, status, clickHandler }: Props) {
         <span className="flex items-center mr-4">{status ? "✅" : "⭕️"}</span>
         <p className="overflow-hidden truncate">{title}</p>
       </div>
-      <time
-        className="block text-sm text-slate-400 font-light text-right truncate leading-6"
-        dateTime={date}
-      >
-        {date}
-      </time>
+
+      <div className="flex items-center justify-end">
+        <time
+          className="block text-sm text-slate-400 font-light text-right truncate leading-6"
+          dateTime={date}
+        >
+          {date}
+        </time>
+        <button
+          type="button"
+          className="ml-4 text-sm leading-6"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            deleteHandler ? deleteHandler(e) : undefined;
+          }}
+        >
+          🗑️
+        </button>
+      </div>
     </article>
   );
 }
--- components/todo/types.d.ts
+++ components/todo/types.d.ts
@@ -3,6 +3,7 @@ type Props = {
   date: string;
   status: boolean;
   clickHandler?: (e: MouseEvent<HTMLElement>) => void;
+  deleteHandler?: (e: MouseEvent<HTMLElement>) => void;
 };
 
 export { Props };
