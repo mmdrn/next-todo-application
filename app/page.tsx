df2dcc182e3e3f89b04086d89ddbd249365412d6
Mohammad RN <mr.nezamdust@gmail.com>
2024-01-31 20:30:57 +0330

    add item and search functionality has been implemented

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import Todo from "@/components/todo";
 import EmptyList from "@/components/empty-list";
 import { useTodoStore } from "@/store/todo";
@@ -13,20 +13,33 @@ export default function Home() {
   const addItem = useTodoStore((state) => state.add);
   const removeItem = useTodoStore((state) => state.remove);
 
+  const [newItemTitle, setNewItemTitle] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
   const uncompletedItems = useMemo(() => {
-    return items.filter((item) => !item.status);
-  }, [items]);
+    return items.filter(
+      (item) =>
+        !item.status &&
+        item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [items, searchQuery]);
 
   const completedItems = useMemo(() => {
-    return items.filter((item) => item.status);
-  }, [items]);
+    return items.filter(
+      (item) =>
+        item.status &&
+        item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [items, searchQuery]);
 
   // methods
   const handleAddNewItem = () => {
     addItem({
-      title: "123",
+      title: newItemTitle,
       status: false,
     });
+
+    setNewItemTitle("");
   };
 
   const handleChangeItemStatus = (id: string) => {
@@ -59,41 +72,74 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-start justify-start w-full">
+    <main className="flex flex-col items-center justify-start gap-10 w-full">
       {/* header */}
-      <header className="border-b border-slate-300 bg-slate-50 p-4 w-full flex items-center justify-start">
-        <h1 className="font-bold mr-10 text-lg text-slate-700">
-          Next Todo Application
-        </h1>
-        <input
-          type="text"
-          className="border border-slate-300 rounded-md w-96 px-2 leading-10 text-sm outline-none focus:border-blue-500 transition-colors"
-          placeholder="Search"
-        />
-        <button
-          type="button"
-          className="rounded-md bg-blue-500 hover:bg-blue-700 transition-colors text-white text-sm px-4 h-10 flex items-center pb-0.5 mr-0 ml-auto"
-          onClick={handleAddNewItem}
-        >
-          <FontAwesomeIcon
-            icon={faCirclePlus}
-            className="w-5 h-5 mr-2 relative top-[1px]"
+      <header className="flex justify-center border-b border-slate-300 bg-slate-50 p-4 w-full">
+        <div className="flex items-center justify-start w-3/4">
+          <h1 className="font-bold mr-10 text-lg text-slate-700">
+            <span className="inline-block mr-2">📝</span> Next Todo Application
+          </h1>
+
+          {/* search input */}
+          <input
+            type="text"
+            className="border border-slate-300 rounded-md w-96 px-2 leading-10 text-sm outline-none focus:border-blue-500 transition-colors"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
           />
-          Add Item
-        </button>
+        </div>
       </header>
 
-      {/* to-do section */}
-      <section className="flex flex-col ml-auto mr-auto w-3/4 mt-10">
-        <h2 className="font-bold mb-3 text-slate-700">To-do</h2>
-        <div className="grid gap-3">{handleRenderItems(false)}</div>
-      </section>
+      <div className="flex flex-col items-center gap-10 justify-center w-3/4">
+        {/* form */}
+        <form
+          className="flex flex-col ml-auto mr-auto w-full"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (newItemTitle) handleAddNewItem();
+          }}
+        >
+          <h2 className="font-bold mb-3 text-slate-700">
+            <span className="inline-block mr-2">✏️</span> Add new item
+          </h2>
+          <div className="grid grid-cols-4 gap-4">
+            <input
+              type="text"
+              className="border border-slate-300 rounded-md px-2 leading-10 h-10 pb-0.5 text-sm outline-none focus:border-blue-500 transition-colors col-span-3"
+              placeholder="Write a to-do"
+              value={newItemTitle}
+              onChange={(e) => setNewItemTitle(e.target.value)}
+            />
 
-      {/* completed section */}
-      <section className="flex flex-col ml-auto mr-auto w-3/4 mt-10">
-        <h2 className="font-bold mb-3 text-slate-700">Completed</h2>
-        <div className="grid gap-3 w-full">{handleRenderItems(true)}</div>
-      </section>
+            <button
+              type="submit"
+              className="w-full max-w-36 text-white text-sm px-4 h-10 flex items-center justify-center pb-0.5 mr-0 ml-auto rounded-md bg-blue-600 enabled:hover:bg-blue-800 transition-colors disabled:cursor-not-allowed disabled:opacity-45"
+              disabled={!!!newItemTitle}
+            >
+              <FontAwesomeIcon
+                icon={faCirclePlus}
+                className="w-5 h-5 mr-2 relative top-[1px]"
+              />
+              Add Item
+            </button>
+          </div>
+        </form>
+
+        {/* to-do section */}
+        <section className="flex flex-col ml-auto mr-auto w-full">
+          <h2 className="font-bold mb-3 text-slate-700">To-do</h2>
+          <div className="grid gap-3">{handleRenderItems(false)}</div>
+        </section>
+
+        {/* completed section */}
+        <section className="flex flex-col ml-auto mr-auto w-full">
+          <h2 className="font-bold mb-3 text-slate-700">Completed</h2>
+          <div className="grid gap-3 w-full">{handleRenderItems(true)}</div>
+        </section>
+      </div>
     </main>
   );
 }
