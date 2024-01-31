ae5eb1ccfd2d6643bf745616690f5645d713871a
Mohammad RN <mr.nezamdust@gmail.com>
2024-01-31 17:30:03 +0330

    a component has been implemented for empty list

---
+++ components/empty-list/index.tsx
@@ -0,0 +1,7 @@
+export default function EmptyList() {
+  return (
+    <div className="rounded-md bg-slate-50 border border-slate-300 p-2 text-center text-sm text-slate-400 font-light">
+      empty list
+    </div>
+  );
+}
